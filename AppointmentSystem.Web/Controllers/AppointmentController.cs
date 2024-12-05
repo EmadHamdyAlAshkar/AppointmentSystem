ada271e121f9b3811272eef907f55c1d11545b3c
OMDA <OMDA@DESKTOP-IGVM1AG>
2024-12-05 21:01:19 +0200

    Make Appointment Updated (Only valid time slots from patient)

--- AppointmentSystem.Web/Controllers/AppointmentController.cs
+++ AppointmentSystem.Web/Controllers/AppointmentController.cs
@@ -38,10 +38,36 @@ namespace AppointmentSystem.Web.Controllers
                     return BadRequest("No available examination time for the doctor !!!");
                 }
 
-                //var existingAppiontment = await _context.Appointments
-                //    .FirstOrDefaultAsync(ap => ap.DoctorId == appointmentDto.DoctorId
-                //                         && ap.AppointmentDate == appointmentDto.AppointmentDate
-                //                         && ap.ExaminationStartTime == appointmentDto.ExaminationStartTime);
+                
+                var doctorAvailability = await _context.DoctorAvailabilities
+                    .FirstOrDefaultAsync(a => a.DoctorId == appointmentDto.DoctorId
+                                             && a.DayOfWeek == appointmentDto.AppointmentDate.DayOfWeek.ToString());
+
+                if (doctorAvailability == null)
+                {
+                    return BadRequest("No available examination time for the doctor !!!");
+                }
+
+                // دى لستة من الاوقات المسموح بيها علشان لما حد يدخل وقت معين هيشوف مناسبة للستة دى ولا لا
+                var allowedStartTimes = new List<TimeSpan>();
+
+                //هنا هحدد البداية بتاعتى علشان هبدا ازود عليها عدد مرات على حسب الدكتور محدد كام دقيقة
+                var time = doctorAvailability.StartTime;
+                while (time < doctorAvailability.EndTime)
+                {
+                    //هنا مثلا لو البداية الساعة 8:00 هيضيفها ف اللستة بتاعت الاوقات المسموحة 
+                    allowedStartTimes.Add(time);
+                    //هنا هيزود المدة اللى الدكتور حاططها على البداية 
+                    time = time.Add(TimeSpan.FromMinutes(doctorAvailability.ExaminationDuration));
+                    //بعدها هيرجع يزود الاوقات المسموحة وهكذا لحد م الوقت يساوى وقت النهاية ساعتها هيقف
+                }
+
+                // هنا هيروح يشوف اذا كان الوقت اللى المريض مدخله متناسب مع اى وقت من اللستة اللى انا محددها
+                if (!allowedStartTimes.Contains(appointmentDto.ExaminationStartTime))
+                {
+                    return BadRequest("The selected start time is not valid based on the doctor's availability and examination duration.");
+                }
+
 
                 var existingAppiontment = await _context.Appointments
                     .FirstOrDefaultAsync(ap => ap.DoctorId == appointmentDto.DoctorId
