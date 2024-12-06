338625392b1d2d8a236ddd76cdbe0cc61fa98ef4
OMDA <OMDA@DESKTOP-IGVM1AG>
2024-12-06 21:35:27 +0200

    Making appointment in order and canclel appointment in order Feature Added

---
+++ AppointmentSystem.Service/AppointmentServices/Dtos/AppointmentInOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSystem.Service.AppointmentServices.Dtos
+{
+    public class AppointmentInOrderDto
+    {
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
--- AppointmentSystem.Web/Controllers/AppointmentController.cs
+++ AppointmentSystem.Web/Controllers/AppointmentController.cs
@@ -17,7 +17,7 @@ namespace AppointmentSystem.Web.Controllers
         {
             _context = context;
         }
-
+        #region Make Appointment With Patient Choise
         [HttpPost]
         public async Task<IActionResult> MakeAppointment(AppointmentDto appointmentDto)
         {
@@ -38,7 +38,7 @@ namespace AppointmentSystem.Web.Controllers
                     return BadRequest("No available examination time for the doctor !!!");
                 }
 
-                
+
                 var doctorAvailability = await _context.DoctorAvailabilities
                     .FirstOrDefaultAsync(a => a.DoctorId == appointmentDto.DoctorId
                                              && a.DayOfWeek == appointmentDto.AppointmentDate.DayOfWeek.ToString());
@@ -74,6 +74,11 @@ namespace AppointmentSystem.Web.Controllers
                                          && ap.AppointmentDate == appointmentDto.AppointmentDate
                                          && ap.ExaminationStartTime == appointmentDto.ExaminationStartTime);
 
+                if (existingAppiontment.PatientId == appointmentDto.PatientId)
+                {
+                    return BadRequest("You have already booked this examination time !!!");
+                }
+
                 if (existingAppiontment != null)
                 {
                     return BadRequest("This examination time is already booked !!!");
@@ -98,7 +103,126 @@ namespace AppointmentSystem.Web.Controllers
 
                 throw new Exception(ex.Message);
             }
-            
+
         }
+        #endregion
+
+        #region Make Appointment With In Ordering
+
+        [HttpPost]
+        public async Task<IActionResult> MakeAppointmentInOrder(AppointmentInOrderDto appointmentDto)
+        {
+            try
+            {
+                var doctorAvailability = await _context.DoctorAvailabilities
+                    .FirstOrDefaultAsync(a => a.DoctorId == appointmentDto.DoctorId
+                                              && a.DayOfWeek == appointmentDto.AppointmentDate.DayOfWeek.ToString());
+
+                if (doctorAvailability == null)
+                {
+                    return BadRequest("No availability found for the doctor on this day.");
+                }
+
+
+                var lastAppointment = await _context.Appointments
+                    .Where(a => a.DoctorId == appointmentDto.DoctorId
+                                && a.AppointmentDate == appointmentDto.AppointmentDate)
+                    .OrderByDescending(a => a.ExaminationStartTime)
+                    .FirstOrDefaultAsync();
+
+
+                var nextStartTime = lastAppointment != null
+                    ? lastAppointment.ExaminationStartTime.Add(TimeSpan.FromMinutes(doctorAvailability.ExaminationDuration))
+                    : doctorAvailability.StartTime;
+
+
+                if (nextStartTime + TimeSpan.FromMinutes(doctorAvailability.ExaminationDuration) > doctorAvailability.EndTime)
+                {
+                    return BadRequest("No available time slots for this day.");
+                }
+
+
+                var appointment = new Appointment
+                {
+                    PatientId = appointmentDto.PatientId,
+                    DoctorId = appointmentDto.DoctorId,
+                    AppointmentDate = appointmentDto.AppointmentDate,
+                    ExaminationStartTime = nextStartTime,
+                    Status = "Booked"
+                };
+
+                _context.Appointments.Add(appointment);
+                await _context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    Message = "Appointment booked successfully",
+                    AppointmentDetails = new
+                    {
+                        DoctorId = appointment.DoctorId,
+                        PatientId = appointment.PatientId,
+                        AppointmentDate = appointment.AppointmentDate,
+                        ExaminationStartTime = appointment.ExaminationStartTime
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        #endregion
+
+        #region Cancel Appointment
+        [HttpPost]
+        public async Task<IActionResult> CancelAppointment(int appointmentId)
+        {
+            try
+            {
+                var appointment = await _context.Appointments.FindAsync(appointmentId);
+
+                if (appointment == null)
+                {
+                    return BadRequest("This appointment is not exist");
+                }
+
+                var appointments = await _context.Appointments
+                                    .Where(ap => ap.AppointmentDate == appointment.AppointmentDate)
+                                    .ToListAsync();
+
+                var doctorAvailability = await _context.DoctorAvailabilities
+                                                .Where(a => a.DoctorId == appointment.DoctorId
+                                                        && appointment.AppointmentDate.DayOfWeek.ToString() == a.DayOfWeek)
+                                                .FirstOrDefaultAsync();
+
+                if (doctorAvailability == null)
+                {
+                    return BadRequest("This doctor availablity not found !!!");
+                }
+
+                foreach (var app in appointments)
+                {
+                    if (app.ExaminationStartTime > appointment.ExaminationStartTime)
+                    {
+                        app.ExaminationStartTime -= TimeSpan.FromMinutes(doctorAvailability.ExaminationDuration);
+                    }
+                }
+
+                _context.Appointments.Remove(appointment);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { Message = $"Appointment Removed" });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+
     }
 }
